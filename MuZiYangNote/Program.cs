8988697a25fe0397efc3824922d3181a8c462f1e
MuZi Yang <xudongyang@139.com>
2019-03-05 11:24:43 +0800

    添加使用期限

--- MuZiYangNote/Program.cs
+++ MuZiYangNote/Program.cs
@@ -11,6 +11,7 @@ namespace MuZiYangNote
     static class Program
     {
         public static string Min = "";
+        private static DateTime Period = Convert.ToDateTime("2019-3-6");
         /// <summary>
         /// 超级管理员代码
         /// </summary>
@@ -27,12 +28,12 @@ namespace MuZiYangNote
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            ExceedATrialPeriod();
             if (IsRunning())
             {
                 MessageBoxEX.Show("木子杨便签已经打开,无法打开多个！");
                 return;
             }
-
             Application.Run(new MdiForm());
         }
         public static bool IsRunning()
@@ -59,6 +60,19 @@ namespace MuZiYangNote
                 }
             }
             return false;
+        }
+        /// <summary>
+        /// 超过使用期
+        /// </summary>
+        public static void ExceedATrialPeriod() {
+            //试用期小于当前日期
+            if (DateTime.Compare(Period, DateTime.Now) < 0) {
+                MessageBoxEX.Show("已超过使用期限！");
+                string path = System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+                Process.Start(path + "Uninstall.exe");
+                Application.Exit();
+            }
+
         }
     }
 }
